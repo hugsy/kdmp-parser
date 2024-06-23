d1a0d1a0738c0dca506b86092adf9c7aeeae998a
crazy hugsy <hugsy@users.noreply.github.com>
2024-06-23 12:47:44 -0700

    Add ARM64 for Linux

--- src/lib/platform.h
+++ src/lib/platform.h
@@ -5,6 +5,8 @@
 #define ARCH_X86
 #elif defined(__amd64__) || defined(_M_X64)
 #define ARCH_X64
+#elif defined(__aarch64__)
+#define ARCH_ARM64
 #else
 #error Architecture not supported.
 #endif
@@ -29,8 +31,10 @@
 
 #if defined ARCH_X86
 #define LINUX_X86
-#elif defined ARCH_X64
+#elif defined(ARCH_X64)
 #define LINUX_X64
+#elif defined(ARCH_ARM64)
+#define LINUX_ARM64
 #endif
 
 #elif defined(__APPLE__)
@@ -38,10 +42,9 @@
 #define SYSTEM_PLATFORM "OSX"
 #if defined ARCH_X86
 #define OSX_X86
-#elif defined ARCH_X64
+#elif defined(ARCH_X64)
 #define OSX_X64
 #endif
-
 #else
 #error An error occured
 #endif
